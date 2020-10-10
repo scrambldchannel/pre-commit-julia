c374db1645da09f4ad6865181f22e70c58699067
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-10 21:53:45 +0200

    draft format hook

---
+++ pre_commit_hooks/format.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# check Julia is in path
+if ! command which julia &>/dev/null; then
+  >&2 echo 'julia not found'
+  exit 1
+fi
+
+julia --color=yes -e  'using Pkg;Pkg.add("JuliaFormatter");using JuliaFormatter;format(".");'
